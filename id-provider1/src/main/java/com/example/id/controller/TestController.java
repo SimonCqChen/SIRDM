1282956f01ebf0eb02727ce400b15494bd262ac6
Chunqi Chen <simoncqchen@outlook.com>
2019-06-25 21:34:54 +0800

    add id microservices provider 1 and 2

---
+++ id-provider1/src/main/java/com/example/id/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.example.id.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @Value("${myww}") // git配置文件里的key
+    String myww;
+
+    @RequestMapping("/hello")
+    public String index() {
+        return "1Hello World" + myww;
+    }
+
+}
---
+++ id-provider2/src/main/java/com/example/id/IdApplication.java
@@ -0,0 +1,15 @@
+package com.example.id;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class IdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdApplication.class, args);
+	}
+
+}
