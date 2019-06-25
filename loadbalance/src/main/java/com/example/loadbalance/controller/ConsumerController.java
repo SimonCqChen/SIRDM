94130c8bc1bc77e1305cd7e47903fdac76a0dee4
Chunqi Chen <simoncqchen@outlook.com>
2019-06-25 21:29:25 +0800

    负载均衡 以服务为单位调用 ribbon会自动读取可用的服务ip进行负载均衡

--- loadbalance/src/main/java/com/example/loadbalance/controller/ConsumerController.java
+++ loadbalance/src/main/java/com/example/loadbalance/controller/ConsumerController.java
@@ -5,6 +5,7 @@ import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
 import java.util.concurrent.ExecutionException;
@@ -14,14 +15,11 @@ public class ConsumerController {
     //注入负载均衡客户端
 
     @Autowired
-    private LoadBalancerClient loadBalancerClient;
+    private RestTemplate restTemplate;
 
     @RequestMapping("/consumer")
     public String helloConsumer() throws ExecutionException, InterruptedException {
-        //这里是根据配置文件的那个providers属性取的
-        ServiceInstance serviceInstance = loadBalancerClient.choose("providers");
-        //负载均衡算法默认是轮询，轮询取得服务
-        URI uri = URI.create(String.format("http://%s:%s", serviceInstance.getHost(), serviceInstance.getPort()));
-        return uri.toString();
+
+        return restTemplate.getForEntity("http://IDENTIFICATION/hello", String.class).getBody();
     }
 }
